e97d3dac7ec735fd7fd5c274c216ae2f04116e62
Hank Chen <cby931001@gmail.com>
2025-06-10 01:25:17 +0800

    註解 route

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,19 @@
-from flask import Flask
-from routes.auth import auth_bp
-from routes.restaurant import restaurant_bp
-from routes.review import review_bp
-from routes.favorite import favorite_bp
-from routes.image import image_bp
-from flask_cors import CORS
+# from flask import Flask
+# from routes.auth import auth_bp
+# from routes.restaurant import restaurant_bp
+# from routes.review import review_bp
+# from routes.favorite import favorite_bp
+# from routes.image import image_bp
+# from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(auth_bp)
-app.register_blueprint(restaurant_bp)
-app.register_blueprint(review_bp)
-app.register_blueprint(favorite_bp)
-app.register_blueprint(image_bp)
+# app.register_blueprint(auth_bp)
+# app.register_blueprint(restaurant_bp)
+# app.register_blueprint(review_bp)
+# app.register_blueprint(favorite_bp)
+# app.register_blueprint(image_bp)
 
 @app.route("/")
 def hello():
